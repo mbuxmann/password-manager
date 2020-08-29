6a782e55a79b50c6d5b2489c97d50718bf770f55
BitVivAZ <jembuxmann@gmail.com>
2020-08-29 21:08:15 +0200

    .

--- main.py
+++ main.py
@@ -1,53 +1,16 @@
-credentials = dict()
+from functions import validate_user, list_commands, add_credential, show_credentials, search_credentials, delete_credential
 greeted = False
-
-
-def validate_user(username, password):
-    if username.lower() == "bitvivaz" and password == "123":
-        return True
-    else:
-        return False
-
-
-def list_commands():
-    print('''
-    a - Add Credentials
-    d - Delete Credentials
-    s - Search Credentials
-    4 - Show All Credentials
-    h - List Commmands
-    e - Exit
-    ''')
-
-
-def add_credentials():
-    website = input("Website: ")
-    username = input("Username: ")
-    password = input("Password: ")
-
-    credentials[website] = {'Username': username, 'Password': password}
-    print(f'{website}: {username} {password} added')
-
-
-def show_credentials():
-    for credential in credentials:
-        print(
-            f"{credential} {credential['Username']} {credential['Password']}")
-
-
-def delete_credentials():
-    website = input("Website: ")
 
 
 if __name__ == "__main__":
     print("Welcome To Your Password Manager")
-    username = input("Username: ")
-    password = input("Password: ")
+    #username = input("Username: ")
+    #password = input("Password: ")
     # logged_in = validate_user(username, password)
     logged_in = True
     while logged_in:
         if not greeted:
-            print(f"\nWelcome back {username}!")
+            #print(f"\nWelcome back {username}!")
             list_commands()
         greeted = True
 
@@ -57,6 +20,8 @@ if __name__ == "__main__":
             add_credentials()
         elif command == 's':
             show_credentials()
+        elif command == 'se':
+            search_credentials()
         elif command == 'd':
             delete_credentials()
         elif command == 'h':
